ff58ee3e6f71057a18f01dce523452d42e5086b8
aryanpuransanaye <aryanpuransanayeh@gmail.com>
2025-03-04 20:14:45 +0330

    Updated README file

--- customers/admin.py
+++ customers/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Customer, Address
 
+
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ('user', 'phone', 'created_at', 'status')
